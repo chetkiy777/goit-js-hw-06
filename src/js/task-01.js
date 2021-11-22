b31f0d39b634eae10af846d22e6384224d6fc08f
Igor Gladkiy <egunka1988@gmail.com>
2021-11-22 10:06:57 +0200

    add work file start

---
+++ src/js/task-01.js
@@ -0,0 +1,19 @@
+const categories = document.querySelector('#categories');
+const categories = document.querySelectorAll('item');
+
+
+
+
+
+// console.log('Number of categories: 3')
+
+// console.log(`Category: Animals`)
+// console.log(`Elements: 4`)
+
+
+// console.log(`Category: Products`)
+// console.log(`Elements: 3`)
+
+
+// console.log(`Category: Technologies`)
+// console.log(`Elements: 5`)

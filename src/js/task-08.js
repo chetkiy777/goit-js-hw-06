6cf8b307cef46f104b4f5f80ffc2a191824ebf06
Igor Gladkiy <egunka1988@gmail.com>
2021-12-17 20:52:26 +0200

    refactor task8
    and add package.json

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,31 +1,49 @@
 const form = document.querySelector('.login-form')
-const buttonSubmit = form.querySelector('button')
 
-const onError = (message) => {
-    alert(message)
-}
-
-
-
-form.addEventListener('submit', (e, ) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault()
-   
-    let emailFieldName = form.elements[0].name;
-    let emailFieldValue = form.elements[0].value;
-    let passwordFieldName = form.elements[1].name;
-    let passwordFieldValue = form.elements[1].value;
+    
+    const { email, password } = e.currentTarget
 
+    
 
     const profile = {
-        [emailFieldName]: emailFieldValue,
-        [passwordFieldName]: passwordFieldValue
+        email: email.value,
+        password: password.value
     }
 
-    if (passwordFieldValue === '') {
-        alert('error')
-        return
+    if (profile.email === '' || profile.password === '') {
+       return alert('error')
     }
 
     console.log(profile)
-    form.reset()
+    form.reset()  
 })
+
+// const buttonSubmit = form.querySelector('button')
+
+// const onError = (message) => {
+//     alert(message)
+// }
+
+// form.addEventListener('submit', (e, ) => {
+//     e.preventDefault()
+   
+//     let emailFieldName = form.elements[0].name;
+//     let emailFieldValue = form.elements[0].value;
+//     let passwordFieldName = form.elements[1].name;
+//     let passwordFieldValue = form.elements[1].value;
+
+//     const profile = {
+//         [emailFieldName]: emailFieldValue,
+//         [passwordFieldName]: passwordFieldValue
+//     }
+
+//     if (passwordFieldValue === '') {
+//         alert('error')
+//         return
+//     }
+
+//     console.log(profile)
+//     form.reset()
+// })

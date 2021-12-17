1735dc8dfa61f2bc3a6d1bc57306b792cf6f3f09
Igor Gladkiy <egunka1988@gmail.com>
2021-12-17 18:09:44 +0200

    Update task-02.js
    change script hw2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -7,16 +7,24 @@ const ingredients = [
   'Condiments',
 ];
 
-const ingredientsContainer = document.querySelector('#ingredients');
+let ingredientsContainer = document.querySelector('#ingredients');
+ 
+ingredients.map(item => {
+  const newElem = document.createElement('li')
+  newElem.classList.add('item')
+  newElem.innerText = item
+  ingredientsContainer.append(newElem)
+})
 
-const makeItem = (itemsArray) => {
-  return itemsArray.map(item => {
-    const itemElem = document.createElement('li');
-    itemElem.innerHTML = item;
-    itemElem.classList.add('item');
-    ingredientsContainer.append(itemElem);
-    return ingredientsContainer.append(itemElem);
-    })  
-}
 
-makeItem(ingredients);
+// const makeItem = (itemsArray) => {
+//   return itemsArray.map(item => {
+//     const itemElem = document.createElement('li');
+//     itemElem.innerHTML = item;
+//     itemElem.classList.add('item');
+//     ingredientsContainer.append(itemElem);
+//     return ingredientsContainer.append(itemElem);
+//     })  
+// }
+
+// makeItem(ingredients);

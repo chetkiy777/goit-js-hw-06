26675cbe12b1f5604ebdc57dd3b394a76f0cba83
Igor Gladkiy <egunka1988@gmail.com>
2021-12-17 23:25:35 +0200

    correct 1task and finish

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,18 @@
-const categories = document.querySelector('#categories');
 const categoriesItem = document.querySelectorAll('.item');
 
-const numberOfCategories = categories.getElementsByTagName('h2').length
-console.log('Number of categories: ', numberOfCategories)
+console.log('Number of categories: ', document.querySelectorAll('h2').length)
 
-const aniamlsEl = categoriesItem[0].querySelector('h2');
-const productsEl = categoriesItem[1].querySelector('h2');
-const technologieslEl = categoriesItem[2].querySelector('h2');
+const animalsCategory = document.querySelectorAll('h2')[0]
+const productsCategory = document.querySelectorAll('h2')[1]
+const technologiesCategory = document.querySelectorAll('h2')[2]
 
-console.log('Category: ', aniamlsEl.textContent);
-console.log('Elements: ', categoriesItem[0].querySelector('ul').children.length);
 
-console.log('Category: ', productsEl.textContent);
-console.log('Elements: ', categoriesItem[1].querySelector('ul').children.length);
+console.log('Category: ', animalsCategory.textContent)
+console.log('Elements: ', categoriesItem[0].querySelectorAll('li').length);
 
-console.log('Category: ', technologieslEl.textContent);
-console.log('Elements: ', categoriesItem[2].querySelector('ul').children.length);
+console.log('Category: ', productsCategory.textContent)
+console.log('Elements: ', categoriesItem[1].querySelectorAll('li').length);
+
+console.log('Category: ', technologiesCategory.textContent)
+console.log('Elements: ', categoriesItem[2].querySelectorAll('li').length);
+

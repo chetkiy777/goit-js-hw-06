da18a0bca4d52e4ecb0a4b61dd5a6ed15ef1edfc
Igor Gladkiy <egunka1988@gmail.com>
2021-12-17 20:12:07 +0200

    task5 remove split

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,6 +2,7 @@ const input = document.querySelector('#name-input')
 const output = document.querySelector('#name-output')
 
 input.addEventListener('input', () => {
-    let letterCount = input.value.split('').length
+    let letterCount = input.value.length
+    console.log(letterCount)
     output.textContent = letterCount === 0 ? "Anonymous" : input.value
 })

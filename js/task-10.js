e49d08ffa944afd547e252d0bbd614dd445c000f
Igor Gladkiy <egunka1988@gmail.com>
2021-12-20 10:50:01 +0200

    refactor task10

--- js/task-10.js
+++ js/task-10.js
@@ -11,13 +11,14 @@ let amount = 0
 const createBoxes = () => {
   amount = input.value
   for (let i = 0; i < amount; i += 1) {
-    let height =  `${30 + i * 10 }`  + 'px'
-    let width = `${30 + i * 10 }`  + 'px'
+    // let height =  `${30 + i * 10 }`  + 'px'
+    // let width = `${30 + i * 10 }` + 'px'
     const div = document.createElement('div')
     div.classList.add('box')
     div.style.background = getRandomHexColor()
-    div.style.height = height
-    div.style.width = width
+    div.style.height = `${30 + i * 10}` + 'px'
+    div.style.width = `${30 + i * 10}` + 'px'
+    
     boxesContainer.append(div)
     console.log(div)
   }
@@ -29,9 +30,5 @@ createBtn.addEventListener('click', () => createBoxes())
 
 // Удаляем всех детей из boxesContainer
 destroyBtn.addEventListener('click', () => {
-  let boxes = boxesContainer.querySelectorAll('.box')
-  for (let i = 0; i < boxes.length; i += 1) {
-    boxesContainer.removeChild(boxes[i])
-  }
-
-})
+  boxesContainer.innerHTML = ''
+  })
